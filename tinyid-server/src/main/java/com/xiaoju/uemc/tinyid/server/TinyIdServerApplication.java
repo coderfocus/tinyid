016ff89911ebb711f401ec03963faee213e48ae4
song <719172036@qq.com>
2021-09-28 16:09:54 +0800

    feat: 整合 Spring Cloud Nacos

--- tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/TinyIdServerApplication.java
+++ tinyid-server/src/main/java/com/xiaoju/uemc/tinyid/server/TinyIdServerApplication.java
@@ -2,6 +2,7 @@ package com.xiaoju.uemc.tinyid.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableAsync
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class TinyIdServerApplication {
 
     public static void main(String[] args) {
